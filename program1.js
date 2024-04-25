cd77f59676f05d1275c23d621b432fab8b042fd2
AadityKumar <91368637+AadityKumar@users.noreply.github.com>
2024-04-25 14:39:31 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,7 +1,30 @@
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
+    let maxLength = 0;      
+    let start = 0;
+    const charIndexMap = {};
+
+    for (let end = 0; end < s.length; end++) {
+        const currentChar = s[end];
+
+        if (charIndexMap[currentChar] !== undefined && charIndexMap[currentChar] >= start) {
+            start = charIndexMap[currentChar] + 1;
+        }
+
+        charIndexMap[currentChar] = end;
+
+        const currentLength = end - start + 1;
+
+        if (currentLength > maxLength) {
+            maxLength = currentLength;
+        }
+    }
+
+    return maxLength;
 }
 
 module.exports = { longestSubstring };
+
+
+
 
 

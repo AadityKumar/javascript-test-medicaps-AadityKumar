a7e5fd2c41799ee7405acb936a74d93c9253e107
AadityKumar <91368637+AadityKumar@users.noreply.github.com>
2024-04-25 14:40:39 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,23 @@
 function smallestMissingPositiveInteger(nums) {
+  const n = nums.length;
 
-  // Implement the function smallest_missing_positive_integer
+  for (let i = 0; i < n; i++) {
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      const temp = nums[nums[i] - 1];
+      nums[nums[i] - 1] = nums[i];
+      nums[i] = temp;
+    }
+  }
 
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) {
+      return i + 1;
+    }
+  }
+
+  return n + 1;
 }
 
 module.exports = smallestMissingPositiveInteger;
+
+
